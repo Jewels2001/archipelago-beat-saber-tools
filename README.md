d81a8dda64f711d05c76cff364e70be85556798f
Julie Wojtiw-Quo <juliewq25@gmail.com>
2026-05-09 04:59:29 +0000

    add: grabs/scrapes least amount of necessary true info

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # archipelago-beat-saber-tools
+
+
+
+## song_scraper.py
+This program scrapes the song files from a zip (as exported from BSManager) and generates a portion of the yaml (`songs`) to be pasted into your archipelago yaml
+
+For versions 0.0.5 and 0.1.0 of [Beat Saber Archipelago](https://github.com/CurrentlyPending/BeatSaberAP)
+
+Works from wherever you have a list of songs 
+- can export to zip, extract, and run this script from that folder
+- can plop this script into your /Beat Saber_Data/CustomLevels folder and run it from there (however I don't recommend leaving the script in there afterwards when you run the game.. it theory it might not affect anything but who knows what the game will do when it sees a random file inside its folder)
+
+
+### steps:
+currently you need to: 
+- **have your songs/maps already downloaded** 
+(ie, in BSManager, if you have a playlist, download the songs from them/ click "synchronize playlist" so that they appear in the maps menu)
+Then click *Export Maps* so that a `.zip` file is generated containing all of the beatmaps/songs. The playlist/.bplist file will **NOT** currently work because it does not contain all of the information needed (the yaml needs the song levelID from beatsaver)
+
+
---
+++ song_scraper.py
@@ -0,0 +1,121 @@
+
+from pathlib import Path
+
+def main():
+    """ Scrapes the .zip (or extracted .zip) for all songs"""
+
+    # in future, can just grab straight from songs folder, ie
+    # C:\Users\[USERNAME]\BSManager\BSInstances\[Version_Name]\Beat Saber_Data\CustomLevels
+    # needs to grab id from song folder
+    # -> needs to check if song is default or not
+
+    # then needs to either
+    # a) scrape names of available difficulty .dat files
+    # or b) scrape data from info.dat
+    # could do both to assert that the files exist (and/or know which files to look for)
+
+
+    # step 1:
+    # get path for songs
+    # assume you are already in correct folder for now (need a check to make sure later)
+    # ie, place script into the folder
+    subdirs = [x for x in Path('.').iterdir() if x.is_dir()]
+    for cur_dir in subdirs:
+        # we have a directory
+        # Step 2: extract necessary info (name, levelid, is_official from here)
+        # assumes each subdirectory has an info.dat
+        # if not, will skip I guess?
+        bundle = {
+            "name": "",
+            "characteristic": "",
+            "difficulty": 0,
+            "is_official": False,
+            "levelid": ""
+        }
+        # 1 - check if it is a (Built In) song
+        #TODO: edge case of song names that contain '-'s (can they contain a - ?)
+        #TODO: change all prints to use logging so we can have debug vs info statements (--verbose)
+        print(cur_dir)
+        dir_name = cur_dir
+        # print(type(dir_name))
+        # print(type(cur_dir))
+        dir_name = str(cur_dir)
+        assert type(dir_name) == str, "if this is not string in your version, need to set it (uncomment previous line)"
+        
+        if "(Built In)" in dir_name:
+            #TODO: custom / edge case levelids being different than song names (could hardcode?)
+            # do logic here to get song name
+            # special ids will need to be processed separately or gathered from elsewhere
+            print(f"Processing Built In song: {dir_name}")
+            full = dir_name.split("(")[0]
+            song_name = full.partition(" - ")[2]
+            song_id = song_name.strip()
+            print(f"Song name: {song_name}")
+            print(f"Song id: {song_id}")
+
+            bundle["name"] = song_name
+            bundle["is_official"] = True
+            bundle["levelid"] = song_id
+
+        else:
+            # do logic here to get id and song name 
+            print(f"Custom song: {dir_name}")
+            song_id = dir_name.split("(")[0].strip()
+            print(f"ID: {song_id}")
+            song_name_auth = dir_name.split("(", 1)[1].rsplit(")", 1)[0]
+            print(f"song name - author: '{song_name_auth}'")
+            # this rsplit(,1) or rpartition *should* deal with song names that contain '-'s by taking the last occurrence
+            # the spaces should help deal with an author's name too.. 
+            # assuming no authors can have spaces / the exact syntax " - " including spaces
+            song_name = dir_name.split("(", 1)[1].rpartition(" - ")[0]
+            print(f"song name if needed: {song_name}")
+
+            # we use the name with authors because we might have different beat maps be the same song by different authors
+            bundle["name"] = song_name_auth
+            bundle["levelid"] = song_id
+
+        # now we just need the characteristic and difficulty
+        # these can be gathered from the info.dat
+        # technically the info.dat lists *all* of the existing characteristics and difficulties
+        # any combination of characteristics and difficulties can theoretically exist as far as I know
+
+        # for now we will scrape always standard if exists and the hardest listed difficulty
+        # archipelago code only currently allows for 0-4 so we will cap/min the difficulty at/with 4
+        # info_dat_path = cur_dir / "Info.dat"
+        # print(info_dat_path)
+        # print(type(info_dat_path))
+
+        # with open(info_dat_path, 'r') as file:
+        #     data = json.load(file)
+        
+
+        # we reformat the bundle here and then "ship" it off to the file 
+        # so it is saved even if this script breaks half way through running
+
+        breakpoint()
+
+            
+
+    # things to return:
+    # - song name
+    # - song chracteristic
+    # - difficulty (if there are multiple just pick the highest?)
+    # - levelid
+
+    pass 
+
+
+def reformat_dict_bundle(bundle):
+    good_dict = {
+        bundle["name"]: {
+            "characteristic": bundle["characteristic"],
+            "difficulty": bundle["difficulty"],
+            "is_official": bundle["is_official"],
+            "levelid": bundle["levelid"]
+        }
+    }
+    return good_dict
+
+
+if __name__ == "__main__":
+    main()
